16a8be33496b311dbb3090b0c2bf385f5bac068f
Gitanjali Bhaiswar <gitanjalibhaiswar@gmail.com>
2024-02-27 19:39:17 +0530

    Pushing Introduction to Javascript Problems - Day 1

---
+++ Day1.js
@@ -0,0 +1,25 @@
+// Using Var
+var a;          // Declaring a variable.
+a = 10;         // Assigning a value to the variable.
+console.log(a); // Running the code.
+
+// Shorthand for declaration and assignment
+var a = 10;
+console.log(a);
+
+
+var x = 12;
+console.log(typeof(x)); // Output: number
+
+var y = "12";
+console.log(typeof(y)); // Output: string
+
+var x; //declaration
+
+x=10; //assigning
+
+console.log(x); //printing to console/shell.
+
+//I can also do
+
+var x=10; //This is basically declaration and assigning together.
